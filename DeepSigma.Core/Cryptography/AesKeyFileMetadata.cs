9176d725735f886c5cccb0fede383eed26492a21
Brenden Perry <brendenperry5@gmail.com>
2026-07-08 12:42:13 -0700

    Add Id to AesKeyFileMetadata and bump version
    Introduce an Id field to AesKeyFileMetadata to uniquely identify AES key files. Update CryptoUtilities to populate the new Id using Guid.NewGuidTimeOrdered().ToString() when generating key files, and bump package Version to 1.6.0.

--- DeepSigma.Core/Cryptography/AesKeyFileMetadata.cs
+++ DeepSigma.Core/Cryptography/AesKeyFileMetadata.cs
@@ -10,8 +10,9 @@ namespace DeepSigma.Core.Cryptography;
 /// <param name="TextEncodingType">The encoding type used for the AES key file.</param>
 /// <param name="EncodingName">The name of the encoding used for the AES key file.</param>
 /// <param name="FileVersion">The version of the AES key file format.</param>
+/// <param name="Id">The unique identifier of the AES key file.</param>
 /// <param name="Description">A description of the AES key file.</param>
-public record AesKeyFileMetadata(int KeySizeInBytes, int IVSizeInBytes, EncodingType TextEncodingType, string EncodingName, string FileVersion, string Description = "AES key file")
+public record AesKeyFileMetadata(int KeySizeInBytes, int IVSizeInBytes, EncodingType TextEncodingType, string EncodingName, string FileVersion, string Id, string Description = "AES key file")
 {
     /// <summary>
     /// Gets the creation timestamp of the AES key file metadata in UTC.
