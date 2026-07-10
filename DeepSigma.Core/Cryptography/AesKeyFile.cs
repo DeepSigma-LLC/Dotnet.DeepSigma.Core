9fdfac74570ba5bb5a65e42ef4130b96a5c3cd44
Brenden Perry <brendenperry5@gmail.com>
2026-07-10 13:57:08 -0700

    Expose AES key/IV bytes; bump version
    Add KeyBytes and InitializationVectorBytes properties to AesKeyFile to return decoded byte[] values by calling Encoder.DecodeFromString with Metadata.TextEncodingType. Add using for DeepSigma.Core.Encode. Bump package version to 1.7.2.

--- DeepSigma.Core/Cryptography/AesKeyFile.cs
+++ DeepSigma.Core/Cryptography/AesKeyFile.cs
@@ -1,4 +1,6 @@
-﻿namespace DeepSigma.Core.Cryptography;
+﻿using DeepSigma.Core.Encode;
+
+namespace DeepSigma.Core.Cryptography;
 
 /// <summary>
 /// Represents an AES key file containing the AES key, initialization vector (IV), and associated metadata.
@@ -6,4 +8,16 @@
 /// <param name="Key"></param>
 /// <param name="InitializationVector"></param>
 /// <param name="Metadata"></param>
-public record AesKeyFile(string Key, string InitializationVector, AesKeyFileMetadata Metadata);
+public record AesKeyFile(string Key, string InitializationVector, AesKeyFileMetadata Metadata)
+{
+    /// <summary>
+    /// Gets the AES key as a byte array by decoding the string using the specified text encoding type from the metadata.
+    /// </summary>
+    public byte[] KeyBytes => Encoder.DecodeFromString(Key, Metadata.TextEncodingType);
+
+    /// <summary>
+    /// Gets the initialization vector (IV) as a byte array by decoding the string using the specified text encoding type from the metadata.
+    /// </summary>
+    public byte[] InitializationVectorBytes => Encoder.DecodeFromString(InitializationVector, Metadata.TextEncodingType);
+}
+
